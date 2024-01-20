2e61fe78cdf9eeaca542691d239d2b1d04ccf04f
Thiago Beck <114706247+IBeckzzzz@users.noreply.github.com>
2024-01-20 19:44:51 -0300

    Update menu-bar.component.ts

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu-bar',
-  templateUrl: './menu-bar.component.html',
-  styleUrls: ['./menu-bar.component.css']
+  templateUrl: 'src/app/components/menu-bar/menu-bar.component.html',
+  styleUrls: ['src/app/components/menu-bar/menu-bar.component.css']
 })
 export class MenuBarComponent implements OnInit {
 
